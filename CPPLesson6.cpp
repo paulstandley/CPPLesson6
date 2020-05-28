869d7a0699b3f856aff813e9e8f883b27aa70944
Paul Standley <paulstandley1972@gmail.com>
2020-05-28 20:59:58 +0100

    quiz for each

--- CPPLesson6.cpp
+++ CPPLesson6.cpp
@@ -13,84 +13,64 @@
 #include <vector>
 
 
+/*
+Declare a fixed array with the following names: Alex, Betty, Caroline, Dave, Emily, 
+Fred, Greg, and Holly. Ask the user to enter a name. Use a for each loop to see if the name 
+the user entered is in the array.
+*/
 
-void for_each_loops_and_references()
+std::string quiz_get_name_string()
 {
-	// In the following for-each example, our element declarations are declared by value
-	std::string array1[]{ "peter", "likes", "frozen", "yogurt" };
-	for (auto element : array1)
-		std::cout << element << ' ';
-	std::cout << '\n';
-	// This means each array element iterated over will be copied into variable element. 
-	// Copying array elements can be expensive, and most of the time we really 
-	// just want to refer to the original element. 
-	// Fortunately, we can use references for this
-	for (auto &element : array1) 
-		std::cout << element << ' ';
-	std::cout << '\n';
-	// The ampersand makes element a reference to the actual array element, 
-	// preventing a copy from being made
-	// In the above example, element will be a reference to the 
-	// currently iterated array element, avoiding having to make a copy. 
-	// Also any changes to element will affect the array being iterated over, 
-	// something not possible if element is a normal variable.
-	// And, of course, it’s a good idea to make your element const if you’re intending 
-	// to use it in a read - only fashion
-	for (const auto &element : array1) // element is a const reference to the currently iterated array element
-		std::cout << element << ' ';
-	// An array that decayed to a pointer cannot be used in a for-each loop
+	while (true)
+	{
+		std::string name{};
+		std::cout << "Enter a name: ";
+		std::getline(std::cin, name);// get full string from cin
+		if (std::cin.fail())
+		{
+			std::cin.clear();// reset cin
+			std::cin.ignore(32767, '\n');// remove input
+		}
+		else
+		{
+			std::cin.ignore(32767, '\n');// remove input
+			return name;
+		}
+	}
 }
 
-void for_each_loops_and_non_arrays()
+void quiz_array_for_each()
 {
-	// note use of std::vector here rather than a fixed array
-	std::vector fibonacci{ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 }; 
-	for (auto number : fibonacci)
-		std::cout << number << ' ';
-	std::cout << '\n';
-	// For-each loops don’t only work with fixed arrays, they work with many kinds of list-like 
-	// structures, such as vectors (e.g. std::vector), linked lists, trees, and maps.
-	// We haven’t covered any of these yet, so don’t worry if you don’t know what these are. 
-	// Just remember that for each loops provide a flexible and generic way to iterate 
-	// through more than just arrays
-	// For-each doesn’t work with pointers to an array
-//#include <iostream>
-//
-//		int sumArray(int array[]) // array is a pointer
-//	{
-//		int sum{ 0 };
-//
-//		for (auto number : array) // compile error, the size of array isn't known
-//			sum += number;
-//
-//		return sum;
-//	}
-//
-//	int main()
-//	{
-//		int array[]{ 9, 7, 5, 3, 1 };
-//
-//		std::cout << sumArray(array) << '\n'; // array decays into a pointer here
-//
-//		return 0;
-//	}
-	// For-each loops do not provide a direct way to get the array index of the current 
-	// element. This is because many of the structures that for-each loops can be used 
-	// with (such as linked lists) are not directly indexable!
-	// For - each loops provide a superior syntax for iterating through an array when we 
-	// need to access all of the array elements in forwards sequential order.
-	// It should be preferred over the standard for loop in the cases where it can be used.
-	// To prevent making copies of each element, the element declaration should ideally
-	// be a reference.
+	const std::string string_array[]{ "Alex", "Betty", "Caroline", "Dave", "Emily", "Fred", "Greg", "Holly" };
+	bool in_array{ false };
+	std::string name{ quiz_get_name_string() };
+	std::cout << name << '\n';
+	for (std::string elements : string_array)
+	{
+		if (elements == name)
+		{
+			std::cout << name << " was found." << '\n';
+			in_array = true;
+			break;
+		}
+	}
+	if (in_array)
+	{
+		std::cout << name << " was found." << '\n';
+	}
+	else
+	{
+		std::cout << name << " was ont found." << '\n';
+	}
 }
-
 
 
 int main()
 {
+	quiz_array_for_each();
 	
-	for_each_loops_and_non_arrays();
-	for_each_loops_and_references();
+	
+
 
     return 0;
 }
